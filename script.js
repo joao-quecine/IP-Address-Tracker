123dcfb5729d91256ba73ee4f530a69f9275c1d6
joao-quecine <quecinejoaopedro@gmail.com>
2024-02-21 17:17:36 -0300

    implementando API para receber os dados do IP

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function getIP(){
             if(xml.status==200){
                 let responseObjJSON= JSON.parse(xml.responseText)
                 input.value=responseObjJSON['ip']
-
+                IPInfo(responseObjJSON['ip'])
             }
             else{
                 alert('Algo deu errado. Codigo do erro: '+xml.status)
@@ -18,3 +18,36 @@ function getIP(){
     xml.send()
 }
 getIP()
+
+
+//API para obter dados do IP
+function IPInfo(ip){
+    let xml= new XMLHttpRequest()
+    xml.open('GET',`http://ip-api.com/json/${ip}`)
+    xml.onreadystatechange=function(){
+        if(xml.readyState==4){
+            if(xml.status==200){
+                let responseObjJSON= JSON.parse(xml.responseText)
+                document.getElementById('IP-Address').innerHTML=responseObjJSON['query']
+                document.getElementById('Location').innerHTML=responseObjJSON['country']+', '+responseObjJSON['city']
+                document.getElementById('Timezone').innerHTML=responseObjJSON['timezone']
+                document.getElementById('ISP').innerHTML=responseObjJSON['isp']
+            }
+            else{
+                alert('Algo deu errado. Codigo do erro: '+xml.status)
+            }
+        }
+    }
+    xml.send()
+}
+
+//adicionando eventListener no botao 
+let button=document.getElementById('search')
+button.addEventListener('click', function(){
+    if(input.value != ''){
+        IPInfo(input.value)
+    }
+    else{
+        alert('insira um IP válido')
+    }
+})
